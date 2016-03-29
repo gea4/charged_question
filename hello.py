d9d12fe024c13501aedea7bbbf3498022e0b4503
Greg Allison <greg.allison95@gmail.com>
2016-03-29 15:28:12 -0500

    Hellow world working\!

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Set up for flask
+DEBUG = True
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# set up for gspread
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+gc = gspread.authorize(credentials)
 
 @app.route('/')
 def hello_world():
-    return 'Greg"s Web app!'
+    sh = gc.open("Charged Question Responses")
+    worksheet = sh.get_worksheet(0)
+    val = worksheet.acell('A1').value
+    return val
 
 if __name__ == '__main__':
     app.run()
